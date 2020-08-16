ed48ff29f10366f17d97cd67f4bf9e43a633da81
João Teixeira <joao@indielayer.com>
2020-08-16 13:15:57 +0100

    docs: add colors and shortkeys on examples

--- dev/serve.vue
+++ dev/serve.vue
@@ -14,67 +14,79 @@ export default Vue.extend({
         url: require('./sounds/petaculo.mp3'),
         volume: 1,
         color: '#11e9e2',
-        shortkey: 'B'
+        shortkey: 'Q'
       }, {
         label: 'Uepaaa',
         url: require('./sounds/uepaaa.mp3'),
         volume: 1,
         color: '#cee811',
-        shortkey: 'N'
+        shortkey: 'W'
       }, {
         label: 'Horn',
         url: require('./sounds/horn.mp3'),
         volume: 0.1,
-        color: '#ed8a10',
-        shortkey: 'M'
+        color: '#b19a47',
+        shortkey: 'E'
       }, {
         url: require('./sounds/1.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#10ed2b',
+        shortkey: 'A'
       }, {
         url: require('./sounds/2.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#ff2300',
+        shortkey: 'S'
       }, {
         url: require('./sounds/3.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#9d00ff',
+        shortkey: 'D'
       }, {
         url: require('./sounds/4.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#ff00f7',
+        shortkey: 'Z'
       }, {
         url: require('./sounds/5.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#004eff',
+        shortkey: 'X'
       }, {
         url: require('./sounds/6.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#0ee31e',
+        shortkey: 'C'
       }, {
         url: require('./sounds/7.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#ed1057',
+        shortkey: 'R'
       }, {
         url: require('./sounds/8.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#ed8a10',
+        shortkey: 'T'
       }, {
         url: require('./sounds/9.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#ed8a10',
+        shortkey: 'Y'
       }, {
         url: require('./sounds/10.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#ed8a10',
+        shortkey: 'F'
       }, {
         url: require('./sounds/11.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#ed8a10',
+        shortkey: 'G'
       }, {
         url: require('./sounds/12.wav'),
         volume: 1,
-        color: '#ed8a10'
+        color: '#ed8a10',
+        shortkey: 'H'
       }]
     }
   }
